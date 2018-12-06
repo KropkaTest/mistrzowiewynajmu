ca88e79f0b07ccf32a9868a0ec378f909b476fe7
KropkaRobert <kropkarobert@gmail.com>
2018-12-06 18:46:58 +0100

    create models

---
+++ MistrzowieWynajmu/MistrzowieWynajmu/Models/Database/Owner.cs
@@ -0,0 +1,10 @@
+﻿namespace MistrzowieWynajmu.Models.Database
+{
+    public class Owner
+    {
+        public int OwnerId { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Phone { get; set; }
+    }
+}
